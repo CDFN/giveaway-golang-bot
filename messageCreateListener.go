9c9dd2b80309f493859dda91e57a24337c2a942c
Bartosz Stefańczyk <cdfn@pop-os.localdomain>
2019-05-17 17:54:39 +0200

    🔥 removing useless code which was used for debugging

--- messageCreateListener.go
+++ messageCreateListener.go
@@ -1,53 +1,41 @@
 package main
 
 import (
-	"github.com/bwmarrin/discordgo"
 	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
-	if message.Content == ""{
+	if message.Content == "" {
 		return
 	}
-	if message.Author.ID == session.State.User.ID{
+	if message.Author.ID == session.State.User.ID {
 		return
 	}
 
 	args := strings.Fields(message.Content)
 
-	if strings.HasPrefix(message.Content, Prefix){
+	if strings.HasPrefix(message.Content, Prefix) {
 
 		switch strings.Trim(strings.ToLower(args[0]), "!") {
-		case "ping":
-			sendMessage(session, message.ChannelID, ":ping_pong: Pong! ")
-		case "reverse", "odwroc":
-			if len(args)<2{
-				return
-			}
-			s := reverse(strings.Join(args[1:], " "))
-			sendMessage(session, message.ChannelID, s)
+		case "help", "pomoc":
+			sendMessage(session, message.ChannelID, "!thx @Mention - Say `thank you` and add mentioned to giveaway. You cant add yourself")
 		case "thx", "thanks", "podziekuj":
-
 			var mention *discordgo.User
 
-			if len(message.Mentions) == 0 || len(args) < 2{
-				sendMessage(session, message.ChannelID, "Musisz oznaczyć osobę, której chcesz podziękować!")
+			if len(message.Mentions) == 0 || len(args) < 2 {
+				sendMessage(session, message.ChannelID, "Mention who you want thank to!")
 				return
-			}else{
+			} else {
 				mention = message.Mentions[0]
 			}
-			if mention.ID == message.Author.ID{
-				sendMessage(session, message.ChannelID, "Nie bądź samolubny!  Nie możesz podziękowac samemu sobie \\:)")
+			if mention.ID == message.Author.ID {
+				sendMessage(session, message.ChannelID, "Dont be like that! You cant thank yourself \\:)")
 				return
 			}
 			Participants = append(Participants, mention.Username)
-			sendMessage(session, message.ChannelID, "Uczestnicy: " + strings.Join(Participants, ", "))
-
-
-
-
-		default:
-			sendMessage(session, message.ChannelID, "Nie znam komendy `tu bedzie komenda`!")
+			sendMessage(session, message.ChannelID, "Participants: "+strings.Join(Participants, ", "))
 
 		}
 
