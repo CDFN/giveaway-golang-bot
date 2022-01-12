3f789b2a87e43b302df4c7625acb3065f893e186
CDFN <codefun@protonmail.com>
2022-01-12 23:52:19 +0100

    More fixing up

--- reactionAddListener.go
+++ reactionAddListener.go
@@ -4,27 +4,28 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"strings"
 )
+
 //❌ ✅
-func reactionAdd(session *discordgo.Session, reaction *discordgo.MessageReactionAdd){
-	for i := 0; i < len(Thxs); i++{
-		if reaction.MessageID == Thxs[i].messageID{
-			member, err := session.State.Member(reaction.GuildID, reaction.UserID)
+func reactionAdd(session *discordgo.Session, reaction *discordgo.MessageReactionAdd) {
+	for i := 0; i < len(Thxs); i++ {
+		if reaction.MessageID == Thxs[i].messageID {
+			member, err := session.GuildMember(reaction.GuildID, reaction.UserID)
 			checkErr("Error while converting userID into Member", err)
-			for _, RoleID := range member.Roles{
-				if RoleID == RoleNeededToAccept{
-					switch reaction.Emoji.Name{
-						case "❌":
-							sendMessage(session, reaction.ChannelID, "Declined "+ Thxs[i].mentioned.Username+ "'s request!")
-							Thxs[i] = Thx{Thxs[i].messageID, Declined, Thxs[i].mentioner, Thxs[i].mentioned}
-						case "✅":
-							sendMessage(session, reaction.ChannelID, "Added " + Thxs[i].mentioned.Username+ " to giveaway!")
-							Thxs[i] = Thx{Thxs[i].messageID, Accepted, Thxs[i].mentioner, Thxs[i].mentioned}
-							sendMessage(session, reaction.ChannelID, "Participants: " + strings.Join(getAllUsernames(getAllConfirmedUsers(Thxs)), ", "))
+			for _, RoleID := range member.Roles {
+				if RoleID == RoleNeededToAccept {
+					switch reaction.Emoji.Name {
+					case "❌":
+						sendMessage(session, reaction.ChannelID, "Declined "+Thxs[i].mentioned.Username+"'s request!")
+						Thxs[i] = Thx{Thxs[i].messageID, Declined, Thxs[i].mentioner, Thxs[i].mentioned}
+					case "✅":
+						sendMessage(session, reaction.ChannelID, "Added "+Thxs[i].mentioned.Username+" to giveaway!")
+						Thxs[i] = Thx{Thxs[i].messageID, Accepted, Thxs[i].mentioner, Thxs[i].mentioned}
+						sendMessage(session, reaction.ChannelID, "Participants: "+strings.Join(getAllUsernames(getAllConfirmedUsers(Thxs)), ", "))
 					}
 					return
 
-				}else{
-					if reaction.UserID == session.State.User.ID{
+				} else {
+					if reaction.UserID == session.State.User.ID {
 						return
 					}
 					//hasn't perm
